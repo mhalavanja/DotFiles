210bf7f88d3385179877d55a7ecf16f2b4117d61
matijahhh <mhalavanja@gmail.com>
2022-11-05 12:38:35 +0100

    Fix waybar

--- .config/waybar/scripts/kblayout.sh
+++ .config/waybar/scripts/kblayout.sh
@@ -1,4 +1,4 @@
-language=`swaymsg -t get_inputs | jq -r '.[1] | .xkb_active_layout_name'`
+language=`swaymsg -t get_inputs | jq -r '.[0] | .xkb_active_layout_name'`
 
 if [[ $language ==  'English (US)' ]]
 then swaymsg input type:keyboard xkb_layout hr
